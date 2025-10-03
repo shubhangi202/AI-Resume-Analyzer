292e9d815762062ecfebfe44dffa2cc9789ff67a
shubhangi202 <shubhangi.singh137@gmail.com>
2025-10-03 15:59:04 +0530

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -7,7 +7,7 @@ import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Resumind" },
+    { title: "Ai-Powered Resume Analyzer" },
     { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
